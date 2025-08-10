c7c340ea28148d665218a3cd656aa2b457049349
jpeguero <jpeguero@gmail.com>
2025-08-10 05:03:21 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 StackBlitz CodeFlow</h1>
+      <p>Deployed on Vercel!</p>
+    </div>
+  );
+}
